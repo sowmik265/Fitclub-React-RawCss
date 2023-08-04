7f35bfbf43aeece1a08eba76dbf2d9baed95c67e
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-08-04 14:27:17 +0600

    hero section done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Hero.css';
 import Header from '../Header/Header';
-
+import hero_image from '../../assets/hero_image.png';
+import hero_image_back from '../../assets/hero_image_back.png';
+import Heart from '../../assets/heart.png';
+import Calories from '../../assets/calories.png';
 
 const Hero = () => {
     return (
@@ -9,12 +12,12 @@ const Hero = () => {
 
             <div className='left-h'>
                 <Header></Header>
-                
+
                 <div className="the-best-ad">
                     <div></div>
                     <span>the best fitness club in the town</span>
                 </div>
-                
+
                 <div className='hero-text'>
                     <div>
                         <span className='stroke-text'>Shape</span>
@@ -31,13 +34,47 @@ const Hero = () => {
                 </div>
 
                 <div className="figures">
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <span>+140</span>
+                        <span>expert coaches</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
                 </div>
+
+                <div className="hero-buttons">
+                    <button className='btn'>Get Started</button>
+                    <button className='btn'>Learn More</button>
+                </div>
+
             </div>
 
-            <div className='right-h'>right side</div>
+            <div className='right-h'>
+                <button className='btn'>Join now</button>
+
+                <div className="heart-rate">
+                    <img src={Heart} alt="" />
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+
+                <img className='hero-image' src={hero_image} alt="" />
+                <img className='hero-image-back' src={hero_image_back} alt="" />
+                
+                <div className="calories">
+                    <img src={Calories} alt="" />
+                    <div>
+                        <span>Calories Burned</span>
+                        <span>220 kcal</span>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
