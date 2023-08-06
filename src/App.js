29ed523db0b7d1fbf91d6ba9bbe3481224ac4603
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-08-06 16:24:41 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Join from './components/Join/Join';
 import Plans from './components/Plans/Plans';
@@ -15,6 +16,7 @@ function App() {
          <Plans></Plans>
          <Testimonials></Testimonials>
          <Join></Join>
+         <Footer></Footer>
     </div>
   );
 }
